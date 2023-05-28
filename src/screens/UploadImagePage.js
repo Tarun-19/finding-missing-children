01200a826709b579d03b5e2cff4ea68fe68e4813
Adarsh Tiwari <you@example.com>
2023-05-28 20:01:55 +0530

    ui changes for upload img+vid

--- src/screens/UploadImagePage.js
+++ src/screens/UploadImagePage.js
@@ -1,14 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../components/brand/brand.css';
+import '../containers/header/header.css';
+import ai from '../assets/ai.png';
+
 
 function UploadImagePage() {
-    const [imageName, setimageName] = useState("test-image");
+  const [imageName, setImageName] = useState("test-image");
+
   return (
-    <div>
-      <h1 style={{color:'red'}}>Upload Image Page</h1>
-      <Link to={"/upload-video/"+imageName}>
-          <button type="button">Next</button>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+      <div className="gpt3__header-image">
+      <img src={ai} alt="AI" className="ai-image" />
+        </div>
+      </div>
+
+      <div className="upload-image-page-content">
+        <h1 className="upload-image-page-title">Upload Image</h1>
+
+        <Link to={`/upload-video/${imageName}`}>
+          <button className="round-button">Next</button>
         </Link>
+      </div>
+      <div className="gpt3__header-image">
+      <img src={ai} alt="AI" className="ai-image" />
+          
+        </div>
     </div>
   );
 }
--- src/screens/UploadVideoPage.js
+++ src/screens/UploadVideoPage.js
@@ -1,18 +1,34 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import '../containers/header/header.css';
+import ai from '../assets/ai.png';
+
 
 function UploadVideoPage() {
     const { imageName } = useParams();
     console.log(imageName)
     const [videoName, setvideoName] = useState("test-video");
   return (
-    <div>
-      <h1 style={{color:'red'}}>Upload video Page</h1>
+    <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+    <div className="gpt3__header-image">
+    <img src={ai} alt="AI" className="ai-image" />
+      </div>
+    </div>
+
+    <div className="upload-image-page-content">
+      <h1 className="upload-image-page-title">Upload video</h1>
+
       <Link to={"/results/"+imageName+"/"+videoName}>
-          <button type="button">Start processing</button>
+        <button className="round-button">Start Poccess</button>
       </Link>
     </div>
+    <div className="gpt3__header-image">
+    <img src={ai} alt="AI" className="ai-image" />
+      </div>
+  </div>
+  
   );
 }
 
